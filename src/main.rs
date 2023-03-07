97a854070e80c90d9d84aa202bee614401b0cdba
xiayulu <harolddfinchh@gmail.com>
2023-03-07 17:54:00 +0800

    fix:bind

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn main() -> std::io::Result<()> {
                     .to(graphql_playground),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
