547b9ad1245a8113abea0ae150ae17ee4a5afb32
xiayulu <harolddfinchh@gmail.com>
2023-03-10 17:46:26 +0800

    feat: refact

--- src/passman/model.rs
+++ src/passman/model.rs
@@ -1,14 +1,14 @@
 use super::schema::{CreateInput, Password, UpdateInput};
 use crate::config::mongo::get_col;
+use crate::error::Result;
 use futures::TryStreamExt;
 use mongodb::bson::{doc, oid::ObjectId};
-use std::io::Error;
 
 fn col_name() -> &'static str {
     "passman"
 }
 
-pub async fn create_password(input: CreateInput) -> Result<Password, Error> {
+pub async fn create_password(input: CreateInput) -> Result<Password> {
     let mut doc = Password {
         _id: None,
         subject: input.subject,
@@ -18,18 +18,14 @@ pub async fn create_password(input: CreateInput) -> Result<Password, Error> {
     };
 
     let col = get_col::<Password>(col_name());
-
-    let data = col
-        .insert_one(doc.clone(), None)
-        .await
-        .expect("Error creating owner");
+    let data = col.insert_one(doc.clone(), None).await?;
 
     doc._id = data.inserted_id.as_object_id();
 
     Ok(doc)
 }
 
-pub async fn update(input: UpdateInput) -> Result<String, Error> {
+pub async fn update(input: UpdateInput) -> Result<String> {
     let col = get_col::<Password>(col_name());
     let obj_id = ObjectId::parse_str(input._id).unwrap();
     let filter = doc! {"_id":obj_id};
@@ -39,44 +35,28 @@ pub async fn update(input: UpdateInput) -> Result<String, Error> {
       "hash":input.hash,
     }};
 
-    let _data = col
-        .update_one(filter, update, None)
-        .await
-        .expect("Error creating owner");
+    let _data = col.update_one(filter, update, None).await?;
 
     Ok("ok".to_owned())
 }
 
-pub async fn get_passwords() -> Result<Vec<Password>, Error> {
+pub async fn get_passwords() -> Result<Vec<Password>> {
     let col = get_col::<Password>(col_name());
-
-    let mut cursors = col
-        .find(None, None)
+    let cursor = col.find(None, None).await?;
+    let data = cursor
+        .try_collect::<Vec<Password>>()
         .await
-        .expect("Error getting list of owners");
-
-    let mut data: Vec<Password> = Vec::new();
-    while let Some(item) = cursors
-        .try_next()
-        .await
-        .expect("Error mapping through cursor")
-    {
-        data.push(item)
-    }
-
+        .unwrap_or(vec![]);
     Ok(data)
 }
 
-pub async fn password_detail(id: &String) -> Result<Password, Error> {
+pub async fn password_detail(id: &String) -> Result<Password> {
     let obj_id = ObjectId::parse_str(id).unwrap();
     let filter = doc! {"_id": obj_id};
 
     let col = get_col::<Password>(col_name());
 
-    let detail = col
-        .find_one(filter, None)
-        .await
-        .expect("Error getting owner's detail");
+    let detail = col.find_one(filter, None).await?;
 
     Ok(detail.unwrap())
 }
